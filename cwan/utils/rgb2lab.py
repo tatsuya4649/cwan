9f11595b0a12424e85b70211a1e1629789560243
tatsuya <tatsuya4946developer@gmail.com>
2020-09-27 21:23:38 +0900

    make rgb2lab tools

--- cwan/utils/rgb2lab.py
+++ cwan/utils/rgb2lab.py
@@ -5,16 +5,111 @@ this file is to decompose the low-light RGB to LAB color space(lightness and col
 """
 import torch
 import torch.nn as nn
-
-class LAB():
+import matplotlib.pyplot as plt
+import cv2
+import numpy as np
+class LAB(nn.Module):
     def __init__(self):
-        super().init()
+        super().__init__()
+    def _check_shape(self,tensor):
+        if tensor.shape[0] != 3:
+            raise ValueError("Input array must have (3,height,width)")
 
-    def rgb2lab_tensor(self,rgb_tensor):
-        pass
-    def rgb2lab_rgb(self,rgb):
-        pass
+    def rgb2xyz(self,rgb_tensor,show_results=False):
+        """RGB to XYZ color space conversion.
+
+        Parameters
+        ==========
+
+        rgb_tensor : shape -> (3,height,width) Tensor
+        show_results : whether to display the resulting xyz image
+
+        Returns
+        ==========
+        
+        xyz_tensor : shape -> (3,height,width) Tensor
+
+        what is xyz_tensor?
+        -------------------
+            -> https://www.dic-color.com/knowledge/xyz.html 
+
+        """
+        self._check_shape(rgb_tensor) #must have input shape {3,height,width}
+        rgb_tensor = rgb_tensor.permute(1,2,0)
+        mask = rgb_tensor > 0.04045
+        rgb_tensor[mask] = torch.pow((rgb_tensor[mask] + 0.055)/1.055,2.4)
+        rgb_tensor[~mask] /= 12.92
+        xyz_from_rgb = torch.tensor([[0.412453, 0.357580, 0.180423],
+                                     [0.212671, 0.715160, 0.072169],
+                                     [0.019334, 0.119193, 0.950227]])
+        xyz = torch.matmul(rgb_tensor,torch.t(xyz_from_rgb))
+        if show_results: # show matplotlib
+            xyz_numpy = xyz.cpu().detach().numpy()
+            plt.imshow(xyz_numpy)
+            plt.show()
+
+        xyz = xyz.permute(2,0,1)
+        return xyz
+
+    def xyz2lab(self,xyz_tensor,show_results=False):
+        """XYZ to CIE-LAB color space conversion.
+
+        Parameters
+        ==========
+
+        xyz_tensor : shape -> (3,height,width) Tensor
+        show_results : whether to display the resulting lab image
+        
+        Returns
+        ==========
+        
+        lab_tensor : shape -> (3,height,width) Tensor
+
+        what is lab_tensor?
+        -------------------
+            -> http://rysys.co.jp/dpex/help_laboutput.html 
+
+
+        """
+        xyz_tensor = xyz_tensor.permute(1,2,0)
+        mask = xyz_tensor > 0.008856
+        xyz_tensor[mask] = torch.pow(xyz_tensor[mask],1/3)
+        xyz_tensor[~mask] = 7.787 * xyz_tensor[~mask] + 16. / 116.
+        x,y,z = xyz_tensor[...,0],xyz_tensor[...,1],xyz_tensor[...,2]
+        L = (116. * y) - 16.
+        a = 500. * (x - y)
+        b = 200. * (y - z)
+        lab = torch.cat([L.unsqueeze(-1),a.unsqueeze(-1),b.unsqueeze(-1)],dim=-1)
+        if show_results:
+            lab_numpy = lab.cpu().detach().numpy()
+            plt.imshow(lab_numpy)
+            plt.show()
+
+        lab = lab.permute(2,0,1)
+        return lab
+
+
+    def forward(self,rgb_tensor,show_xyz_results=False,show_lab_results=False):
+        xyz = self.rgb2xyz(rgb_tensor,show_xyz_results)
+        lab = self.xyz2lab(xyz,show_lab_results)
+        l = lab[0:1]
+        ab = lab[1:]
+        return l,ab
 
 if __name__ == "__main__":
     print("Hello,rgb2lab.py!!!")
+    
+    _IMAGE_PATH = "../../sample_images/silhouette-3038483_1920-1280x640.jpg"
+    _DEFAULT_SIZE = 512
+    im = cv2.imread(_IMAGE_PATH)
+    im = cv2.cvtColor(im,cv2.COLOR_BGR2RGB)#BGR->RGB
+    height = im.shape[0]
+    width = im.shape[1]
+    print("image_height -> {}".format(height))
+    print("image_width -> {}".format(width))
+    im = im[height//2-_DEFAULT_SIZE//2:height//2+_DEFAULT_SIZE//2,width//2-_DEFAULT_SIZE//2:width//2+_DEFAULT_SIZE//2]
     lab = LAB()
+    rgb_image = torch.from_numpy(im.transpose(2,0,1)).float()
+    rgb_image /= 255.
+    l,ab = lab(rgb_image,True,True)
+
--- install.sh
+++ install.sh
@@ -5,14 +5,7 @@ sudo apt-get update
 sudo apt-get install build-essential python3-pip python3-venv
 sudo ln -s /usr/bin/pip3 /usr/bin/pip # to move pip command
 
-echo "making virtual environments..."
-echo "=============================="
-
 sudo ln -s /usr/bin/python3 /usr/bin/python # to move python command
-
-python -m venv cwan_venv # make virtual environments
-
-source cwan_venv/bin/activate # activate
 
 echo "install the required libraries..."
 echo "================================="

18a3704c4ed9acfc489148789b997e5b8eddd7cc
tatsuya <tatsuya4946developer@gmail.com>
2020-10-03 22:46:20 +0900

    train dataset

--- README.md
+++ README.md
@@ -2,4 +2,7 @@
 
 CWAN is LLIE(low-light image enhancement) model.this repository is unofficial.
 
+#Requirements
 
+* 10GB~ RAM
+* Dataset(self made)
--- cwan/train_ab.py
+++ cwan/train_ab.py
@@ -13,6 +13,7 @@ import glob
 from tqdm import tqdm
 from utils.loss_huber import loss_huber
 from utils.attention_points import to_attention_points
+from dataset.get_dataset import *
 
 parser = argparse.ArgumentParser(description="train cwan model")
 
@@ -48,45 +49,37 @@ short_dict64 -> data of RGB(list)
 short_image -> imageid and patch of data of RGB(list)
 
 """
-patch_tensor = torch.rand(1,3,64,64)
-long_data = torch.rand(1,3,64,64)
-long_attention_map = torch.rand(1,2,64,64)
-long_binary_points,long_attention_points = to_attention_points(long_attention_map)
-ab_long = lab(long_data)[:,1:3,:,:]
-print(ab_long.shape)
-_,attention_map,attention_points,_,ab_output = cwan(patch_tensor)
-#attention_maps loss
-loss_map = loss_func(attention_map,long_attention_map)
-#huber loss
-loss_huber = loss_huber(ab_output,ab_long)
-#mse loss
-loss_mse = (loss_mse_func(attention_points,long_attention_points) * long_binary_points)/args.beta
-loss_ab = loss_huber + args.alpha * loss_mse
-#total loss
-loss = loss_map + loss_ab
-raise ValueError("a")
 _BATCH = args.batch_size
+dataset = Dataset(32)
+long_dic = dict()
 for e in tqdm(range(args.epochs)):
     print("now {} epoch".format(e))
-    patch_tensor = torch.rand(1,3,64,64)
-    long_data = torch.rand(1,3,64,64)
-    long_attention_map = torch.rand(1,2,64,64)
-    long_binary_points,long_attention_points = to_attention_points(long_attention_map)
-    ab_long = lab(long_data)[:,1:3,:,:]
-    _,attention_map,attentnion_points,_,ab_output = cwan(patch_tensor)
-    #attention_maps loss
-    loss_map = loss_func(attention_map,long_attention_map)
-    #huber loss
-    loss_huber = loss_huber(ab_output,long_ab)
-    #mse loss
-    loss_mse = (loss_mse_func(attention_points,long_attention_points) * Bp)/args.beta
-    loss_ab = loss_huber + args.alpha * loss_mse
-    #total loss
-    loss = loss_map + loss_ab
-    optimizer.zero_grad()
-    loss.backward()
-    optimizer.step()
-
+    while(True):
+        dataset.plus()
+        if dataset.change_now_check:
+            long_dic = dataset.long_dataset()
+        short_imageid_list,short_list = dataset.dataset_tensor()
+        for i in range(_ONE_FILE_SIZE/_BATCH):
+            patch_tensor = short_list[i*_BATCH:(i+1)*_BATCH]
+            long_data = torch.rand(1,3,64,64)
+            long_attention_map = torch.rand(1,2,64,64)
+            long_binary_points,long_attention_points = to_attention_points(long_attention_map)
+            ab_long = lab(long_data)[:,1:3,:,:]
+            _,attention_map,attentnion_points,_,ab_output = cwan(patch_tensor)
+            #attention_maps loss
+            loss_map = loss_func(attention_map,long_attention_map)
+            #huber loss
+            loss_huber = loss_huber(ab_output,long_ab)
+            #mse loss
+            loss_mse = (loss_mse_func(attention_points,long_attention_points) * Bp)/args.beta
+            loss_ab = loss_huber + args.alpha * loss_mse
+            #total loss
+            loss = loss_map + loss_ab
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+        if dataset.check_end():
+            break
     #save model parameters
     state_dict = cwan.cwan_ab.state_dict()
     for key in state_dict.keys():

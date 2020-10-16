2b105ba8b6767febbae329cdcc359ad426b0655c
tatsuya <tatsuya4946developer@gmail.com>
2020-10-16 17:09:41 +0900

    train_l change

--- cwan/net/mem.py
+++ cwan/net/mem.py
@@ -21,20 +21,22 @@ class MemoryBlock(nn.Module):
 class ResidualBlock(nn.Module):
     def __init__(self,channels,k=3,s=1,p=1):
         super().__init__()
-        self.relu_conv1 = BNReLUConv(channels,channels,k,s,p)
-        self.relu_conv2 = BNReLUConv(channels,channels,k,s,p)
+        self.relu_conv1 = ReLUConv(channels,channels,k,s,p)
+        self.relu_conv2 = ReLUConv(channels,channels,k,s,p)
+        self.relu_conv3 = ReLUConv(channels,channels,k,s,p)
     def forward(self,x):
         residual = x
         out = self.relu_conv1(x)
         out = self.relu_conv2(out)
+        out = self.relu_conv3(out)
         out = out + residual
         return out
 
 class ReLUConv(nn.Sequential):
     def __init__(self,in_channels,channels,k=3,s=1,p=1,inplace=True):
         super().__init__()
-        self.add_module('relu',nn.ReLU(inplace=inplace))
         self.add_module('conv',nn.Conv2d(in_channels,channels,k,s,p,bias=False))
+        self.add_module('relu',nn.ReLU(inplace=inplace))
 
 class BNReLUConv(nn.Sequential):
     def __init__(self,in_channels,channels,k=3,s=1,p=1,inplace=True):
